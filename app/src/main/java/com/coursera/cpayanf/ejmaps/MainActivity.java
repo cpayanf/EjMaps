8b28fa3e7b7c44723b21aec1a5cdd865abff7a02
Carlos Payan <carlos.payan@delaware-software.com>
2018-03-02 11:07:12 -0600

    Mapas

--- app/src/main/java/com/coursera/cpayanf/ejmaps/MainActivity.java
+++ app/src/main/java/com/coursera/cpayanf/ejmaps/MainActivity.java
@@ -1,7 +1,9 @@
 package com.coursera.cpayanf.ejmaps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	public void IrAMapa(View v){
+		Intent iMap = new Intent(this, MapsActivity.class);
+		startActivity(iMap);
 	}
 }
